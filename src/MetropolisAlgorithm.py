1f003f09bb761eb4fcb872caa1b1baefbf8db903
Jonah Post <48297677+JonahPost@users.noreply.github.com>
2021-03-26 14:26:44 +0100

    code to accept newstate

--- src/MetropolisAlgorithm.py
+++ src/MetropolisAlgorithm.py
@@ -13,7 +13,11 @@ class MetropolisAlgorithm:
         self.state = np.random.choice(a=[True, False], size=self.size**self.dimensions, p=[self.initial_distribution,1-self.initial_distribution])
         self.state = np.reshape(self.state, newshape=np.repeat(self.size, self.dimensions))
         
-    def new_state(self):
+    def make_new_state(self):
         coordinate = np.random.randint(0,self.size,self.dimensions)
-        self.newstate = np.copy(self.state)
-        self.newstate[coordinate] = not self.newstate[coordinate]
+        self.new_state = np.copy(self.state)
+        self.new_state[coordinate] = not self.new_state[coordinate]
+    
+    def simulation(self):
+        make_new_state()
+        if pobability_distribution(self.new_state) > pobability_distribution(self.state):
