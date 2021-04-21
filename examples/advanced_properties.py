15a73a5184d256fa86a1ed3988048c55ef6fa374
Jonah Post <48297677+JonahPost@users.noreply.github.com>
2021-04-22 00:28:15 +0200

    added figures

--- examples/advanced_properties.py
+++ examples/advanced_properties.py
@@ -8,14 +8,14 @@ settings = {
 	"size": 50,						# Size along axis of grid
 	"dimensions": 2,				# Dimensions of the grid
 	"initial_distribution": 0.75,	# Chance to have spin down in initialization
-	"tmin": 1,						# Minimum temperature
-	"tmax": 4,						# Maximum temperature
-	"t_step_size": 0.2,				# Stepsize of temperature
-	"equilibrize_sweep_length": 5,	# Sublength after which it checks if it is in equilibrium
-	"tau_sweeps": 50,				# Length over which correlation function is calculated (does not scale well!)
-	"N_tau": 25,					# Number of samples to determine correlation length
+	"tmin": 2,						# Minimum temperature
+	"tmax": 2.5,						# Maximum temperature
+	"t_step_size": 0.03,				# Stepsize of temperature
+	"equilibrize_sweep_length": 10,	# Sublength after which it checks if it is in equilibrium
+	"tau_sweeps": 60,				# Length over which correlation function is calculated (does not scale well!)
+	"N_tau": 15,					# Number of samples to determine correlation length
 	"max_blocks": 50,				# Maximum number of blocks of 16tau to calculate the properties over
-	"treshold": 5e-7,				# Flatness of slope after which we call it equillibrilized
+	"treshold": 5e-8,				# Flatness of slope after which we call it equillibrilized
 	"max_sweeps": 100,				# Maximum sweeps after which to cancell atempt to equillibrilize
 	"sweep_length": 10,				# Sweep length of equilibrilization process before checking slope
 	"plot": 0						# Plot magnetization before continuing? (1 or 0)
@@ -68,6 +68,6 @@ def main(settings:dict, root_path:str) -> None:
 
 if __name__ == "__main__":
 	# Quick? Uncomment for quick (testing purposes only!):
-	settings = quick_settings
+# 	settings = quick_settings
 	fpath = f"generated/data"
 	main(settings, fpath)
--- src/data_analysis.py
+++ src/data_analysis.py
@@ -7,6 +7,7 @@ Created on Wed Apr 21 14:56:03 2021
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from src.IO_utils import load_json
 
 # def generate_data(reduced_temp, a, b):
 #     data= powerlaw(reduced_temp, a, b)
@@ -44,12 +45,57 @@ def find_critical_exponents(reduced_temperature, quantity_data, error_quantity_d
 def exact_magnetization(temperature):
     return (1. - (np.sinh(2./temperature))**-4 )**(1./8.)
 
+path="generated\\data\\2021-04-21t224624583630\\"
+filename_tau= "taut.npy"
+filename_m = "mt.npy"
 filename_chi= "chit.npy"
-chi, error_chi = np.load(filename_chi)[:,0],np.load(filename_chi)[:,1]
+filename_c = "ct.npy"
+
+settings = load_json(path, "settings.json")
+# temperature = np.arange(settings["tmin"], settings["tmax"] + settings["t_step_size"]/2., settings["t_step_size"])
+temperature = np.arange(settings["tmin"], settings["tmax"], settings["t_step_size"])
+tau, error_tau = np.load(path+filename_tau)[:,0],np.load(path+filename_tau)[:,1]
+magnetization, error_magnetization = np.load(path+filename_m)[:,0],np.load(path+filename_m)[:,1]
+magnetic_suscepticility, error_magnetic_suscepticility = np.load(path+filename_chi)[:,0],np.load(path+filename_chi)[:,1]
+specific_heat, error_specific_heat = np.load(path+filename_c)[:,0],np.load(path+filename_c)[:,1]
+
+continuous_step_size = 0.001*settings["t_step_size"]
+continuous_temperature = np.append(np.arange(settings["tmin"], 2.265, continuous_step_size), np.arange(2.265,2.27,0.00000001))
+
+m="."
+ls=""
+cs=5
 
 plt.figure()
-plt.plot(chi)
-plt.plot(error_chi)
+plt.errorbar(x=temperature, y=tau, yerr= error_tau, marker=m, linestyle=ls, capsize=cs)
+plt.xlabel(r"Temperature $T$")
+plt.ylabel(r"correlation time $\tau$")
+plt.savefig("figures\\correlation_time.png")
+
+plt.figure()
+plt.errorbar(x=temperature, y=magnetization, yerr= error_magnetization, marker=m, linestyle=ls, capsize=cs, label="MC Ising Model")
+plt.plot(continuous_temperature, exact_magnetization(continuous_temperature), label="Exact solution")
+plt.xlabel(r"Temperature $T$")
+plt.ylabel(r"magnetization $m$")
+plt.legend()
+plt.axis(ymin=0)
+plt.savefig("figures\\magnetization.png")
+
+plt.figure()
+plt.errorbar(x=temperature, y=magnetic_suscepticility, yerr= error_magnetic_suscepticility, marker=m, linestyle=ls, capsize=cs)
+plt.xlabel(r"Temperature $T$")
+plt.ylabel(r"magnetic susceptibility $\chi_M$")
+plt.axis(ymin=0)
+plt.savefig("figures\\magnetics_susceptibility.png")
+
+plt.figure()
+plt.errorbar(x=temperature, y=specific_heat, yerr= error_specific_heat, marker=m, linestyle=ls, capsize=cs)
+plt.xlabel(r"Temperature $T$")
+plt.ylabel(r"Specific heat $C$")
+plt.axis(ymin=0)
+plt.savefig("figures\\specific_heat.png")
+
+
 plt.show()
 
 
